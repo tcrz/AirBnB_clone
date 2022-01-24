ab8e784664bb6da397d648fbf244e53afba43e52
Tonycrz <tonybaidoo0@gmail.com>
2022-01-24 21:18:31 +0000

    BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+BaseModel class that defines all common attributes/methods for other classes
+"""
+from datetime import datetime
+import uuid
+
+
+class BaseModel():
+    """Base Model"""
+    def __init__(self):
+        """initialize instance of BaseModel class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """class object represented as a string"""
+        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute 'updated_at'
+        with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        obj_dict = self.__dict__
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict.update({'created_at': obj_dict['created_at'].isoformat()})
+        obj_dict.update({'updated_at': obj_dict['updated_at'].isoformat()})
+        return obj_dict
