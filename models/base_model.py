f42c2bdda02c61cc09fa6803e0f24452d0cced3e
mickiy123 <mikikinfe345@gmail.com>
2022-01-25 13:10:31 +0300

    Added a class

--- models/base_model.py
+++ models/base_model.py
@@ -5,35 +5,36 @@ import uuid
 import datetime
 import json
 
-def __init__(id, created_at, updated_at):
-    """ Base Init.
+class BaseModel:
+
+    def __init__(id, created_at, updated_at):
+        """ Base Init.
 
         Args:
             id: unique id of instance
             created_at: creation time of instance
             updated_at: modifcation time of the instance
-    """
-    self.id = str(uuid.uuid4())
-    self.created_at = datetime.datetime.now()
-    self.updated_at = datetime.datetime.now()
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
-def __str__(self):
-    """return string representation of the object """
-    return "[{}] ({}) {}".format(
-            self.__class.__name__, self.id, self.__dict__)
+    def __str__(self):
+        """return string representation of the object """
+        return "[{}] ({}) {}".format(
+                self.__class.__name__, self.id, self.__dict__)
 
-def save(self):
-    """ updates the public instance attribute updated_at with the
-    current datetime 
-    """
+    def save(self):
+        """ updates the public instance attribute updated_at with the
+        current datetime 
+        """
+        self.updated_at = datetime.datetime.now()
 
-    self.updated_at = datetime.datetime.now()
-
-def to_dict(self):
-    """ returns a dictionary containing all keys/values of the instance """
-    self.__dict__['__class__'] = self.__class.__name__
-    self.__dict__['created_at'] = self.created_at.isofromat()
-    self.__dict__['updated_at'] = self.updated_at.isofromat()
-    return self.__dict__
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of the instance """
+        self.__dict__['__class__'] = self.__class.__name__
+        self.__dict__['created_at'] = self.created_at.isofromat()
+        self.__dict__['updated_at'] = self.updated_at.isofromat()
+        return self.__dict__
 
 
