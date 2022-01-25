62920ce0ca8015fdccc58a69f46034747d3debff
mickiy123 <mikikinfe345@gmail.com>
2022-01-25 15:18:56 +0300

    updated code style

--- models/base_model.py
+++ models/base_model.py
@@ -11,15 +11,11 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """initialize instance of BaseModel class"""
         if kwargs:
-            for k, v in kwargs.items():
-                if k == 'id':
-                    self.id = v
-                if k == 'created_at':
-                    self.created_at = datetime.strptime(v,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-                if k == 'updated_at':
-                    self.updated_at = datetime.strptime(v,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            self.id = kwargs['id']
+            self.created_at = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -29,6 +25,7 @@ class BaseModel():
         """class object represented as a string"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
+
     def save(self):
         """updates the public instance attribute 'updated_at'
         with the current datetime"""
