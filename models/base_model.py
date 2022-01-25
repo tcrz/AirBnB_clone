7ade86f27c16edaba995022e8c8588c0ed8376af
Tonycrz <tonybaidoo0@gmail.com>
2022-01-25 11:41:19 +0000

    added args and kwargs to init

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,27 @@ import uuid
 
 class BaseModel():
     """Base Model"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialize instance of BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    self.id = v
+                if k == 'created_at':
+                    self.created_at = datetime.strptime(v,
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                if k == 'updated_at':
+                    self.updated_at = datetime.strptime(v,
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """class object represented as a string"""
-        return "[{}] ({}) {}".format(self.__class__.name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute 'updated_at'
