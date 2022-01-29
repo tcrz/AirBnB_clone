01581c47a2410aa6edf8b09a56ae5dfe6ec87781
Tonycrz <tonybaidoo0@gmail.com>
2022-01-29 13:38:07 +0000

    tests for amenity and review classes

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Unittest for Amenity Class
+"""
+import contextlib
+from models.amenity import Amenity
+import unittest
+from datetime import datetime
+from io import StringIO
+
+
+class TestBaseClass(unittest.TestCase):
+    def test_classtype(self):
+        """tests class type"""
+        amenity = Amenity()
+        self.assertIsInstance(amenity, Amenity)
+
+    def test_attr(self):
+        """test attributes"""
+        amenity = Amenity()
+        uuid_val = amenity.id
+        self.assertEqual(amenity.id, uuid_val)
+        self.assertIsInstance(amenity.id, str)
+        created_time = amenity.created_at
+        updated_time = amenity.updated_at
+        self.assertEqual(created_time, amenity.created_at)
+        self.assertEqual(updated_time, amenity.updated_at)
+
+    def test_attr_kwargs(self):
+        """tests instance init with kwargs"""
+        amenity = Amenity()
+        amenity.name = "New Model"
+        amenity.my_number = 201
+        amenity_json = amenity.to_dict()
+        my_new_model = Amenity(**amenity_json)
+        self.assertFalse(amenity is my_new_model)
+        self.assertEqual(my_new_model.name, "New Model")
+        self.assertEqual(my_new_model.my_number, 201)
+        # unfinished
+
+    def test_save_method(self):
+        """test save method"""
+        amenity = Amenity()
+        amenity.name = "prototype"
+        amenity.number = 1
+        amenity.save()
+        update_time = amenity.updated_at
+        self.assertEqual(update_time, amenity.updated_at)
+        with self.assertRaises(TypeError):
+            amenity.save(None)
+
+    def test_str_method(self):
+        """test str method"""
+        temp_stdout = StringIO()
+        with contextlib.redirect_stdout(temp_stdout):
+            amenity = Amenity()
+            obj_print = amenity.__str__()
+            print(amenity)
+        output = temp_stdout.getvalue().strip()
+        # print(output)
+        self.assertEqual(output, obj_print)
+
+    def test_to_dict(self):
+        """test to_dict method"""
+        amenity = Amenity()
+        self.assertTrue(type(amenity.to_dict()) is dict)
+        objdict = amenity.to_dict()
+        self.assertEqual(objdict, amenity.to_dict())
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Unittest for Review Class
+"""
+import contextlib
+from models.review import Review
+import unittest
+from datetime import datetime
+from io import StringIO
+
+
+class TestBaseClass(unittest.TestCase):
+    def test_classtype(self):
+        """tests class type"""
+        review = Review()
+        self.assertIsInstance(review, Review)
+
+    def test_attr(self):
+        """test attributes"""
+        review = Review()
+        uuid_val = review.id
+        self.assertEqual(review.id, uuid_val)
+        self.assertIsInstance(review.id, str)
+        created_time = review.created_at
+        updated_time = review.updated_at
+        self.assertEqual(created_time, review.created_at)
+        self.assertEqual(updated_time, review.updated_at)
+
+    def test_attr_kwargs(self):
+        """tests instance init with kwargs"""
+        review = Review()
+        review.name = "New Model"
+        review.my_number = 201
+        review_json = review.to_dict()
+        my_new_model = Review(**review_json)
+        self.assertFalse(review is my_new_model)
+        self.assertEqual(my_new_model.name, "New Model")
+        self.assertEqual(my_new_model.my_number, 201)
+        # unfinished
+
+    def test_save_method(self):
+        """test save method"""
+        review = Review()
+        review.name = "prototype"
+        review.number = 1
+        review.save()
+        update_time = review.updated_at
+        self.assertEqual(update_time, review.updated_at)
+        with self.assertRaises(TypeError):
+            review.save(None)
+
+    def test_str_method(self):
+        """test str method"""
+        temp_stdout = StringIO()
+        with contextlib.redirect_stdout(temp_stdout):
+            review = Review()
+            obj_print = review.__str__()
+            print(review)
+        output = temp_stdout.getvalue().strip()
+        # print(output)
+        self.assertEqual(output, obj_print)
+
+    def test_to_dict(self):
+        """test to_dict method"""
+        review = Review()
+        self.assertTrue(type(review.to_dict()) is dict)
+        objdict = review.to_dict()
+        self.assertEqual(objdict, review.to_dict())
