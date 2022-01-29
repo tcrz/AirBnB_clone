f83a4d41f4b42799ae3ce9dc5792af3e765c8a05
Tonycrz <tonybaidoo0@gmail.com>
2022-01-29 11:38:48 +0000

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,31 +23,31 @@ class FileStorage():
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        # if os.path.exists(FileStorage.__file_path):
-        new_data = dict((k, v.to_dict())
-                        for k, v in FileStorage.__objects.items())
-        # with open(FileStorage.__file_path) as file:
-        #     obj_data = json.load(file)
-        #     obj_data.update(new_data)
-        #     with open(FileStorage.__file_path, mode='w') as file:
-        #         json.dump(new_data, file, indent=4)
-        # else:
-        #     obj_data = dict((k, v.to_dict())
-        #                     for k, v in FileStorage.__objects.items())
-        with open(FileStorage.__file_path, mode='w') as file:
-            json.dump(new_data, file, indent=4)
+        if os.path.exists(FileStorage.__file_path):
+            new_data = dict((k, v.to_dict())
+                            for k, v in FileStorage.__objects.items())
+            with open(FileStorage.__file_path) as file:
+                obj_data = json.load(file)
+                obj_data.update(new_data)
+                with open(FileStorage.__file_path, mode='w') as file:
+                    json.dump(new_data, file, indent=4)
+        else:
+            obj_data = dict((k, v.to_dict())
+                            for k, v in FileStorage.__objects.items())
+            with open(FileStorage.__file_path, mode='w') as file:
+                json.dump(obj_data, file, indent=4)
 
     def reload(self):
         """deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists"""
         from models.base_model import BaseModel
         from models.user import User
+        classes = {'BaseModel': BaseModel, 'User': User}
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path) as file:
                 json_str = json.load(file)
             for k, v in json_str.items():
-                # if v["__class__"] in globals():
-                #     classname = globals()[v["__class__"]]
-                FileStorage.__objects[k] = BaseModel(**v)
+                if v["__class__"] in classes:
+                    FileStorage.__objects[k] = classes[v["__class__"]](**v)
         else:
             pass
