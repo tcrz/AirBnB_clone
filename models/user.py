df627a3962c4539f522f2d86e121266a8b4c7edc
Tonycrz <tonybaidoo0@gmail.com>
2022-01-29 10:10:42 +0000

    User class

--- models/user.py
+++ models/user.py
@@ -4,12 +4,15 @@ User class that inherits from BaseModel
 """
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
     """User Class"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
     def __init__(self):
         """initializing User instance"""
         super().__init__()
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+       
