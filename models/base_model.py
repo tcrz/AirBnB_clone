93a7c5b2f2c7f4b4a77a65a92c8cb624495d81bb
Tonycrz <tonybaidoo0@gmail.com>
2022-01-29 14:14:07 +0000

    update?

--- models/base_model.py
+++ models/base_model.py
@@ -17,18 +17,8 @@ class BaseModel():
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key == 'created_at':
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
+                if key != '__class__' and key != 'created_at' and key != 'updated_at':
                     setattr(self, key, value)
-                # if key == 'created_at':
-                #     self.
-            #     if key != 'updated_at' and key != 'created_at'
-            # and key != '__class__':
-            #         setattr(self, key, value)
-            # self.created_at = datetime.strptime(
-            #         kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            # self.updated_at = datetime.strptime(
-            #         kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
