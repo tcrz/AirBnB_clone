3450ca0fc91b706a520c6a0d2f8b8bcd30cda5ba
Tonycrz <tonybaidoo0@gmail.com>
2022-01-29 13:27:18 +0000

    test for state class

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Unittest for State Class
+"""
+import contextlib
+from models.state import State
+import unittest
+from datetime import datetime
+from io import StringIO
+
+
+class TestBaseClass(unittest.TestCase):
+    def test_classtype(self):
+        """tests class type"""
+        state = State()
+        self.assertIsInstance(state, State)
+
+    def test_attr(self):
+        """test attributes"""
+        state = State()
+        uuid_val = state.id
+        self.assertEqual(state.id, uuid_val)
+        self.assertIsInstance(state.id, str)
+        created_time = state.created_at
+        updated_time = state.updated_at
+        self.assertEqual(created_time, state.created_at)
+        self.assertEqual(updated_time, state.updated_at)
+
+    def test_attr_kwargs(self):
+        """tests instance init with kwargs"""
+        state = State()
+        state.name = "New Model"
+        state.my_number = 201
+        state_json = state.to_dict()
+        my_new_model = State(**state_json)
+        self.assertFalse(state is my_new_model)
+        self.assertEqual(my_new_model.name, "New Model")
+        self.assertEqual(my_new_model.my_number, 201)
+        # unfinished
+
+    def test_save_method(self):
+        """test save method"""
+        state = State()
+        state.name = "prototype"
+        state.number = 1
+        state.save()
+        update_time = state.updated_at
+        self.assertEqual(update_time, state.updated_at)
+        with self.assertRaises(TypeError):
+            state.save(None)
+
+    def test_str_method(self):
+        """test str method"""
+        temp_stdout = StringIO()
+        with contextlib.redirect_stdout(temp_stdout):
+            state = State()
+            obj_print = state.__str__()
+            print(state)
+        output = temp_stdout.getvalue().strip()
+        # print(output)
+        self.assertEqual(output, obj_print)
+
+    def test_to_dict(self):
+        """test to_dict method"""
+        state = State()
+        self.assertTrue(type(state.to_dict()) is dict)
+        objdict = state.to_dict()
+        self.assertEqual(objdict, state.to_dict())
