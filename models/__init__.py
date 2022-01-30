9f3aa905980f6f34154e60f8bdd772e6a7648734
Tonycrz <tonybaidoo0@gmail.com>
2022-01-30 16:49:07 +0000

    add imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """this is the init file"""
-
 from models.engine.file_storage import FileStorage
+from .base_model import BaseModel
+from .user import User
+from .review import Review
+from .city import City
+from .amenity import Amenity
+from .place import Place
+from .state import State
 
 storage = FileStorage()
 storage.reload()
