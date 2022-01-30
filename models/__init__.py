c04361a8ad5589fa10c08485930072deaa0630c8
Tonycrz <tonybaidoo0@gmail.com>
2022-01-30 17:05:19 +0000

    changes

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,6 @@
-#!/usr/bin/python3
-"""this is the init file"""
-from models.engine.file_storage import FileStorage
-from .base_model import BaseModel
-from .user import User
-from .review import Review
-from .city import City
-from .amenity import Amenity
-from .place import Place
-from .state import State
+# #!/usr/bin/python3
+# """this is the init file"""
+# from models.engine.file_storage import FileStorage
 
-storage = FileStorage()
-storage.reload()
+# storage = FileStorage()
+# storage.reload()
