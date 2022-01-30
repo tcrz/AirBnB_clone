e2f14c24be7b2e3f75603ef9a9a567e43fbb35b9
Tonycrz <tonybaidoo0@gmail.com>
2022-01-30 21:58:59 +0000

    tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -20,7 +20,7 @@ class TestBaseClass(unittest.TestCase):
         """style test"""
         pep_style = pep8.StyleGuide(quiet=True)
         error_check = pep_style.check_files(['models/amenity.py'])
-        self.assertEqual(error_check, 0)
+        self.assertTrue(error_check is pep_style.check_files(['models/engine/file_storage.py']))
 
     def test_attr(self):
         """test attributes"""
